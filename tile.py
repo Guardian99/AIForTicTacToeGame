278cd3e2544be1bdeda3e91f6d02eb80e4d5f4c7
Kanishk Rana <32814336+Guardian99@users.noreply.github.com>
2018-10-08 14:43:24 +0530

    Added Comments for better readibility

--- tile.py
+++ tile.py
@@ -3,6 +3,7 @@ d=0
 move1=0
 move2=0
 move=0
+#these are tiles of tic tac toe
 tile1=0
 tile2=0
 tile3=0
@@ -12,7 +13,7 @@ tile6=0
 tile7=0
 tile8=0
 tile9=0
-
 player1=1
 player2=2
+#this is the assignment of first chance which is by default given to player 1
 turn=player1
